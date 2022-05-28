ce3fe8297ed95dee953371b74d4322973f7b60cd
Shumov Denis <denis-shumoff@yandex.ru>
2022-05-28 05:45:19 +0300

    Update app.java
    Add methods

--- src/main/java/ru/shumovdenis/App.java
+++ src/main/java/ru/shumovdenis/App.java
@@ -1,13 +1,58 @@
 package ru.shumovdenis;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class App {
+
+    private static final String SEP = File.separator;
+    private static String resultFileName = "result.txt";
+    private static String resultPath = "result" + SEP;
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> filesMap = new HashMap<>();
+        File dir = new File("Files");
+
+        if (dir.isDirectory()) {
+            for (File item : dir.listFiles()) {
+                if (item.isDirectory()) {
+                    for (File txtFiles : item.listFiles()) {
+                        filesMap.put(txtFiles.getName(), txtFiles.getPath());
+                    }
+                } else {
+                    filesMap.put(item.getName(), item.getPath());
+                }
+            }
+        }
+
+        TreeMap<String, String> sortedMap = new TreeMap<>(filesMap);
+        sortedMap.entrySet();
+
+        File resultDir = new File(resultPath);
+        if (!resultDir.exists()) {
+            resultDir.mkdir();
+        }
+
+        for (Map.Entry<String, String> entry : sortedMap.entrySet()) {
+            String filePath = entry.getValue();
+            writeInResult(filePath);
+        }
+    }
+
+    public static void writeInResult(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(resultPath + resultFileName, true))) {
+            String s;
+            while ((s = br.readLine()) != null) {
+                bw.write(s);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка во время копирования " + e.getMessage());
+        }
     }
 }
+
